f109f9ea8c14fe0010165c3079ea26fba2740c4c
kiririmode <kiririmode@gmail.com>
2016-04-23 22:54:41 +0900

    PoolingClientConnectionManager を使用する
    この実装だと、2 本の HTTP コネクションが適切に開放されず、
    20 本のうち 18 本のスレッドは待ちになってしまう

--- src/main/java/com/kiririmode/blog/http/client/ResteasyClient.java
+++ src/main/java/com/kiririmode/blog/http/client/ResteasyClient.java
@@ -3,40 +3,51 @@ package com.kiririmode.blog.http.client;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.jboss.resteasy.client.ClientExecutor;
 import org.jboss.resteasy.client.ClientRequest;
-import org.jboss.resteasy.client.ClientResponse;
+import org.jboss.resteasy.client.core.executors.ApacheHttpClient4Executor;
 
 public class ResteasyClient {
 
-	public ClientResponse<?> get(String uri) throws Exception {
-		return new ClientRequest(uri).get();
-	}
-
 	public static void main(String... args) throws Exception {
 		final int threadNum = 20;
-		final String uri = "http://192.168.99.100:8080";
+		final String uri = "http://192.168.99.100:8081";
+		
+		ClientConnectionManager connManager = new PoolingClientConnectionManager();
+		HttpClient client = new DefaultHttpClient(connManager);
 
 		ExecutorService service = Executors.newFixedThreadPool(threadNum);
 		for (int i = 0; i < threadNum; ++i) {
-			service.submit(new GetRunnable(uri));
+			service.submit(new GetRunnable(uri, client));
 		}
+
+		service.shutdown();
+		Thread.sleep(10000L);
 	}
 
 	public static class GetRunnable implements Runnable {
 
 		private String uri;
+		private HttpClient client;
 
-		public GetRunnable(String uri) {
+		public GetRunnable(String uri, HttpClient client) {
 			this.uri = uri;
+			this.client = client;
 		}
 
 		public void run() {
+			ClientExecutor executor = new ApacheHttpClient4Executor(client);
 			try {
-				new ClientRequest(uri).get();
+				new ClientRequest(uri, executor).get();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 
 	}
+
 }
