c075ef2fb5deb9ffc7f69c6f21fbc2e119a3c603
kiririmode <kiririmode@gmail.com>
2016-04-23 21:48:22 +0900

    RestClient のデフォルトでの振舞
    20 スレッドで同時に HTTP 要求を行うと 20 本の TCP コネクションが張られ、
    5 秒間 (Apache の設定) ESTABLISHED のままとなり、次に CLOSE_WAIT に遷移
    する

--- src/main/java/com/kiririmode/blog/http/client/ResteasyClient.java
+++ src/main/java/com/kiririmode/blog/http/client/ResteasyClient.java
@@ -1,21 +1,42 @@
 package com.kiririmode.blog.http.client;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.jboss.resteasy.client.ClientRequest;
 import org.jboss.resteasy.client.ClientResponse;
 
 public class ResteasyClient {
-	
+
 	public ClientResponse<?> get(String uri) throws Exception {
 		return new ClientRequest(uri).get();
 	}
-	
-	public static void main(String ... args ) throws Exception {
-		ResteasyClient client = new ResteasyClient();
-		
-		ClientResponse<?> response = client.get("http://192.168.99.100:8080");
-		System.out.println(response.getEntity(String.class));
-		
-		Thread.sleep(10000L);
+
+	public static void main(String... args) throws Exception {
+		final int threadNum = 20;
+		final String uri = "http://192.168.99.100:8080";
+
+		ExecutorService service = Executors.newFixedThreadPool(threadNum);
+		for (int i = 0; i < threadNum; ++i) {
+			service.submit(new GetRunnable(uri));
+		}
 	}
 
+	public static class GetRunnable implements Runnable {
+
+		private String uri;
+
+		public GetRunnable(String uri) {
+			this.uri = uri;
+		}
+
+		public void run() {
+			try {
+				new ClientRequest(uri).get();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 }
